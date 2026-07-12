caaeeec1f27d332fcfaedbe06ca223553eb3f8df
Boutaina El Hourri <elhourriboutaina@gmail.com>
2026-07-12 20:55:56 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,29 @@
+require("@nomicfoundation/hardhat-ethers");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "https://rpc.sepolia.org",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111
+    },
+    // Local testing network
+    hardhat: {
+      chainId: 31337
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
+};
